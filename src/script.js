138c8003f001916b5fc599c456090c4c3b231979
JoshD24 <147768968+JoshD24@users.noreply.github.com>
2024-04-04 22:37:31 -0600

    css edits

--- src/script.js
+++ src/script.js
@@ -6,12 +6,12 @@ function createCardElement(item) {
         <li class="card">
             <img src=${item.image} alt="">
             <div class="card-content">
+            <h3 class="header">
+            ${item.title}
+            </h3>
                 <p class="subheader">
                     ${item.subtitle}
                 </p>
-                <h3 class="header">
-                    ${item.title}
-                </h3>
             </div>
         </li>
       `;
